d0b7d72095fa9cea2db3f6270e6d84da677dcef4
CMCmike <102331645+CMCmike@users.noreply.github.com>
2022-09-05 18:15:12 -0400

    Add files via upload

---
+++ controllers/toWatch.js
@@ -0,0 +1,13 @@
+const Todo = require('../models/Todo')
+
+module.exports = {
+getToWatch: async (req,res)=>{
+    console.log(req.user)   
+    try{
+        const toWatch = await Todo.find({toWatch:true})
+        const countToWatch = await Todo.countDocuments({userId:req.user.id, toWatch: true})
+        res.render('watchList.ejs', {allWatchListShows: toWatch, countToWatch: countToWatch, user: req.user})
+    }catch(err){
+        console.log(err)
+    }
+}}
